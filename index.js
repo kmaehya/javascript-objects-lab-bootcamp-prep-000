06d66a74b688ece62c583ab6483d1cd2bfde6f39
Kevin Choong <kmaehya@gmail.com>
2018-06-14 21:27:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key value) {
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
